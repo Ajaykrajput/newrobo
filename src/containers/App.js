e79fa15fab4fbf6b7b885a5fcde46d3b007e0737
Ajay <ajayrajput7rcr@gmail.com>
2021-07-20 15:52:45 +0530

    build app in Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,92 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../component/Cardlist";
 import SearchBox from "../component/SearchBox";
 import Scroll from "../component/Scroll";
-import ErrorBoundry from "../component/ErrorBoundry";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-    // console.log('constructor');
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
+  const [count, setCount] = useState(0);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-    // console.log("componentDidMount")
-  }
-
-  onSearchchange = (event) => {
-    this.setState({ searchfield: event.target.value });
+      .then((users) => setRobots(users));
+    console.log(count);
+  }, [count]); ///   only run if count changes
+  const onSearchchange = (event) => {
+    setSearchfield(event.target.value);
   };
-  render() {
-    const { robots, searchfield } = this.state;
-    const filterdRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    // console.log("render")
-    return !robots.length ? (
-      <h1> Loading ...</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1"> RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchchange} />
-        <Scroll>
-          <ErrorBoundry>
-            <CardList robots={filterdRobots} />
-          </ErrorBoundry>
-        </Scroll>
-      </div>
-    );
-  }
+  console.log(robots, searchfield);
+  const filterdRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+  // console.log("render")
+  return !robots.length ? (
+    <h1> Loading ...</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1"> RoboFriends</h1>
+      <button onClick={() => setCount(count + 1)}>Click Me !!</button>
+      <SearchBox searchChange={onSearchchange} />
+      <Scroll>
+        <CardList robots={filterdRobots} />
+      </Scroll>
+    </div>
+  );
 }
+
 export default App;
+
+/////////////// This is the same code for class Component
+
+// import React, { Component } from "react";
+// import CardList from "../component/Cardlist";
+// import SearchBox from "../component/SearchBox";
+// import Scroll from "../component/Scroll";
+// import ErrorBoundry from "../component/ErrorBoundry";
+// import "./App.css";
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       robots: [],
+//       searchfield: "",
+//     };
+//     // console.log('constructor');
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) => this.setState({ robots: users }));
+//     // console.log("componentDidMount")
+//   }
+
+//   onSearchchange = (event) => {
+//     this.setState({ searchfield: event.target.value });
+//   };
+//   render() {
+//     const { robots, searchfield } = this.state;
+//     const filterdRobots = robots.filter((robot) => {
+//       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+//     });
+//     // console.log("render")
+//     return !robots.length ? (
+//       <h1> Loading ...</h1>
+//     ) : (
+//       <div className="tc">
+//         <h1 className="f1"> RoboFriends</h1>
+//         <SearchBox searchChange={this.onSearchchange} />
+//         <Scroll>
+//           <ErrorBoundry>
+//             <CardList robots={filterdRobots} />
+//           </ErrorBoundry>
+//         </Scroll>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
